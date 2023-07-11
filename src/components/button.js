8465ca065771f7a1f99ecd9d29dfcdbf0da78ddf
Parm <parmjeetmishra2@gmail.com>
2023-07-12 00:36:50 +0530

    BTN components

--- src/components/button.js
+++ src/components/button.js
@@ -1,8 +1,38 @@
-import propTypes from "prop-types";
-import { classNames} from './classNames';
-import React from 'react'
+import { VariantProps, cva } from "class-variance-authority";
+import { FC, cn } from "react";
+import React from "react";
+import 
 
-function Button (className, children, rounded, variant, ...props) {
-    <button className={classNames("rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none", className)} {...props}>{children}</button>
-}
-export default Button;
+const buttonVariants = cva(
+  "rounded-full flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none",
+  {
+    variants: {
+      variant: {
+        default: "",
+        submit:
+          "rounded-full flex items-center bg-orange-500 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none",
+      },
+      size: {
+        default: "h-20 w-20",
+        sm: "h-16, w-16",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+// function Button (className, children) {
+//     <button>{children}</button>
+// }
+// interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>,
+// VariantProps<typeof buttonVariants>
+// {};
+
+const Button: FC<ButtonProps> = ({className, size, variant, ...props }) => {
+  return <button className={cn(buttonVariants({variant,size, className}))} {...props}/>;
+};
+
+export { Button, buttonVariants };
